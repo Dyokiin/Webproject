287904200f352e5f1e845184ecbe62f5b941b2a8
Théo Arnauld Des Lions <arnaulddeslion@univ-eiffel.fr>
2022-02-22 17:59:30 +0100

    [22/02/2022] Work

---
+++ lolproject/src/components/champ_card.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="champ-card">
+    <img class="picture" src="https://my-best-kennel.com/pictures/45.png"/>
+    <div>
+      <h2 class="firstname">Pluto</h2>
+      <p class="breed">Saint-Hubert</p>
+    </div>  
+  </div>
+</template>
+
+<script>
+	export default {
+	  name: 'champ-card'
+	}
+</script>
+
+<style>
+	.champ-card {
+	  display: flex;
+	}
+
+	h2 {
+		 font-size: 20px;
+	}
+</style>
---
+++ lolproject/src/components/champ_gallerie.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="champ-gallery">
+		<champ-card/>
+  </div>
+</template>
+
+<script>
+import champ_card from './components/champ_card.vue'
+
+export default {
+  name: 'ChampGallerie',
+	components: {
+		champ_card
+	}
+}
+</script>
+
+<style></style>
